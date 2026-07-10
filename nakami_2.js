8bd72f910a009177e9df919942d01b17c1562627
nobitaxx2016 <86908855+nobitaxx2016@users.noreply.github.com>
2026-07-10 19:01:29 +0700

    Update headers and body replacements in nakami_2.js
    Replace occurrences of '26.07.2' with '26.27.2' in headers and body.

--- nakami_2.js
+++ nakami_2.js
@@ -1,1 +1,19 @@
+let headers = $request.headers;
 
+// Replace trong Header
+for (let key in headers) {
+    if (typeof headers[key] === "string") {
+        headers[key] = headers[key].replace(/26\.07\.2/g, "26.27.2");
+    }
+}
+
+// Replace trong Body
+let body = $request.body;
+if (body) {
+    body = body.replace(/26\.07\.2/g, "26.27.2");
+}
+
+$done({
+    headers,
+    body
+});

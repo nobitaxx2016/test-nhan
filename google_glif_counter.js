b24bdbf5667cfd30f0bb23f0268b482da90a4b89
nobitaxx2016 <86908855+nobitaxx2016@users.noreply.github.com>
2026-07-31 13:06:49 +0700

    Update google_glif_counter.js

--- google_glif_counter.js
+++ google_glif_counter.js
@@ -46,7 +46,7 @@ if (url === "https://loon.local/counter/off") {
 // =========================
 // Chỉ xử lý request Google
 // =========================
-if (!url.includes("//data/batchexecute")) {
+if (!url.includes("/data/batchexecute")) {
     $done({});
     return;
 }

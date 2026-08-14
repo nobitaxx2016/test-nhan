b2dd8cff22f4b1cef27d24d35a236f4023dfa01f
nobitaxx2016 <86908855+nobitaxx2016@users.noreply.github.com>
2026-08-15 05:27:39 +0700

    Modify response body to include new URL structure
    Updated the response body with a new URL and modified the structure of the body content.

--- nakami_6.js
+++ nakami_6.js
@@ -4,8 +4,14 @@
 
 const url = $request.url;
 const headers = $response.headers || {};
-// Giữ nguyên body từ response gốc
-const body = $response.body;
+
+// Nội dung body đã được cập nhật URL mới
+const newBody = `)]}'
+
+1290
+[["wrb.fr","ihzRS","[[2,[null,[null,null,\\"https://api.wuking3868.pro/test\\\\u0026flowName\\\\u003dGlifSetupSafariVC\\\\u0026client_id\\\\u003d936475272427.apps.googleusercontent.com\\\\u0026tmpl\\\\u003dsearch\\\\u0026auth_extension\\\\u003dADa53XLd2a4mOUndnH_ectqM3AKcQg8-F3p09wEBMFKMK0RHwnXJ8jgW-sYpA-0Y3gQ9EdvCuEEM_XE_aV2xShEj6SlUyEI6BOXDW3HLn-q3zptD-04wnSI\\\\u0026redirect_uri\\\\u003dcom.google.sso.1086610230652-dcccrp116a653pr51iqrine19ponon0b:/authCallback?login%3Dcode\\\\u0026scope\\\\u003dhttps://www.google.com/accounts/OAuthLogin+https://www.googleapis.com/auth/userinfo.email\\\\u0026delegated_client_id\\\\u003d1086610230652-dcccrp116a653pr51iqrine19ponon0b.apps.googleusercontent.com\\\\u0026chtml\\\\u003dLoginDoneHtml\\\\u0026continue\\\\u003dhttps://accounts.google.com/SafariVCRedirect?redirect_uri%3Dcom.google.sso.1086610230652-dcccrp116a653pr51iqrine19ponon0b:/authCallback?login%253Dcode%2526authorization_code%253D4/0AXEQxIBhZgof_aKUzDh8JkcB3QvCiVyBiHesgTfg1unM1stfn5a3HsBVRiUgRTRAId1J7Q\\\\u0026gidl\\\\u003dEgIIAA\\"]]]]",null,null,null,"generic"],["di",363],["af.httprm",363,"8696530325090284105",34]]
+26
+[["e",4,null,null,1328]]`;
 
 function getHeader(name) {
     for (const k in headers) {
@@ -39,7 +45,7 @@ if (setLogin === "logged-in" || googleSignin) {
 
     $done({
         headers: headers,
-        body: body // Trả lại body gốc không thay đổi
+        body: newBody
     });
 
 } else {

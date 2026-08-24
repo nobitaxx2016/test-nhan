e64d2a94ee583d17551d0bd198093ceb6f9b9f5d
nobitaxx2016 <86908855+nobitaxx2016@users.noreply.github.com>
2026-08-24 20:07:03 +0700

    Add safari_authentication_session to body

--- authadvice.js
+++ authadvice.js
@@ -46,6 +46,7 @@ if (!urlRegex.test(url)) {
 
             // Thay đổi
             body.device_id = newDeviceId;
+            body.safari_authentication_session = false;
 
             console.log("========== DEVICE ID ==========");
             console.log("Old: " + oldDeviceId);

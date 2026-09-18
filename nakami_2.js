de748abdcead7b5b7dd35089375305b927fa52a7
nobitaxx2016 <86908855+nobitaxx2016@users.noreply.github.com>
2026-09-18 14:08:22 +0700

    Update version number in headers and body

--- nakami_2.js
+++ nakami_2.js
@@ -3,14 +3,14 @@ let headers = $request.headers;
 // Replace trong Header
 for (let key in headers) {
     if (typeof headers[key] === "string") {
-        headers[key] = headers[key].replace(/26\.07\.2/g, "26.27.2");
+        headers[key] = headers[key].replace(/26\.07\.2/g, "26.37.3");
     }
 }
 
 // Replace trong Body
 let body = $request.body;
 if (body) {
-    body = body.replace(/26\.07\.2/g, "26.27.2");
+    body = body.replace(/26\.07\.2/g, "26.37.3");
 }
 
 $done({

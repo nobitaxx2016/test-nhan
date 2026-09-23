5c624e5b75cdff5ff5e83f7b1d22c3f2c6d7d2f2
nobitaxx2016 <86908855+nobitaxx2016@users.noreply.github.com>
2026-09-23 17:53:58 +0700

    Update NID value in change_nid.js

--- change_nid.js
+++ change_nid.js
@@ -1,7 +1,7 @@
 let cookie = $request.headers["Cookie"] || $request.headers["cookie"];
 
 if (cookie && /(?:^|;\s*)NID=/.test(cookie)) {
-    const newNID = "NID_GIA_TRI_CUA_BAN";
+    const newNID = "Cr8CCAES8QEBp2sxropTzr7TkZN_DKxB3V38_d9WtVffnF4RveeIWvneNIbVkfRtiaQ0heCBG_twtiE8htRD6EKxUnW76su9fPCFY0deqsdSDPsZ1m9FdPJPVlFYLzGto_H-nz6JS26FwaYvFnp3WIqrDH93zRAAR8WUN6TxAuSxNRc6cbJvT9Uv7Taimay2FKXReRTtny-iD-KGYvccgMnCCGn2m89het8B86Mr62auDYEi3o5FTn9K8_WK7vSubUkpKyOGjW-pbxxMRXqvJCVYf6SLqTk_0ixXy-O3uCZw0pi4axufkxfOetBKIm-T7i4OnNnPcLUlKAEyRQEOK94R0uidx139HL79n6MNJ5k9UB6kS8cVpLgjM9YPcA21mrAbMhGMX_O6anu3XuxHIWGvJKIu5QYuVudtR65sNZGTAQ";
 
     cookie = cookie.replace(
         /(^|;\s*)NID=[^;]*/,
